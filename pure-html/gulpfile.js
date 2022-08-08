9e22fd763917cb5b3c07a04f28d16834a14785f6
feizheng <1290657123@qq.com>
2022-08-08 15:21:15 +0800

    fix: tailwind watch task

--- pure-html/gulpfile.js
+++ pure-html/gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require("gulp");
 const { TailwindRegistry, CleanRegistry } = require("@jswork/gulp-registry");
-const reg1 = new TailwindRegistry({src: "./styles/index.scss", dst: "./public/",});
-const reg2 = new CleanRegistry({ target: "./public/" });
+const reg1 = new TailwindRegistry({
+  src: "./styles/index.scss",
+  dest: "./public/",
+});
+const reg2 = new CleanRegistry({ dest: "./public/" });
 
 [reg1, reg2].forEach((reg) => gulp.registry(reg));
+
+gulp.task("tailwind:watch", () => {
+  gulp.watch(["./styles/**/*.scss", "index.html"], gulp.series("tailwind"));
+});
