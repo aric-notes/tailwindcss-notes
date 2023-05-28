df8ebcccc905c7a09e976a5133f93b57359b3282
aric <1290657123@qq.com>
2023-05-28 14:06:27 +0800

    feat: add sass dist

--- pure-html/gulpfile.js
+++ pure-html/gulpfile.js
@@ -2,12 +2,14 @@ const gulp = require("gulp");
 const { TailwindRegistry, CleanRegistry } = require("@jswork/gulp-registry");
 const reg1 = new TailwindRegistry({
   src: "./styles/index.scss",
-  dest: "./public/",
+  dst: "./public/",
 });
 const reg2 = new CleanRegistry({ dest: "./public/" });
 
 [reg1, reg2].forEach((reg) => gulp.registry(reg));
 
-gulp.task("tailwind:watch", () => {
-  gulp.watch(["./styles/**/*.scss", "index.html"], gulp.series("tailwind"));
+gulp.task("dev:tailwind", () => {
+  gulp.watch(["./styles/**/*.scss"], gulp.series("tailwind"));
 });
+
+gulp.task("build:tailwind", gulp.series("clean", "tailwind"));
