2b040ca6530c0e2d187f9877cd3715b5558dd8ab
aric <1290657123@qq.com>
2024-02-25 12:58:50 +0800

    feat: grid-item to css components

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,11 @@ function App() {
   return (
     <div className="mx-auto mt-20 flex w-1/2 flex-col bg-slate-200 p-10">
       <GridList>
-        <div className="border border-solid border-slate-500 bg-gray-200 p-2">
-          item1
-        </div>
-        <div className="border border-solid border-slate-500 bg-gray-200 p-2">
-          item1
-        </div>
-        <div className="border border-solid border-slate-500 bg-gray-200 p-2">
-          item1
-        </div>
-        <div className="border border-solid border-slate-500 bg-gray-200 p-2">
-          item1
-        </div>
-        <div className="border border-solid border-slate-500 bg-gray-200 p-2">
-          item1
-        </div>
+        <div className="grid-item">item1</div>
+        <div className="grid-item">item1</div>
+        <div className="grid-item">item1</div>
+        <div className="grid-item">item1</div>
+        <div className="grid-item">item1</div>
       </GridList>
     </div>
   );
