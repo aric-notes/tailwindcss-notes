3c0ab941b8ba7c89cdad1d0cebbe05863519985e
aric <1290657123@qq.com>
2024-04-09 08:49:01 +0800

    feat: add dynmaic create styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import GridList from './components/grid-list';
+import VarComp from './components/var-comp';
 
 function App() {
   return (
-    <div className="mx-auto mt-20 flex w-1/2 flex-col bg-slate-200 p-10">
+    <div className="mx-auto mt-20 flex w-1/2 flex-col bg-gray-200 p-10">
       <GridList>
         <div className="grid-item">item1</div>
         <div className="grid-item">item1</div>
@@ -10,6 +11,11 @@ function App() {
         <div className="grid-item">item1</div>
         <div className="grid-item">item1</div>
       </GridList>
+      <div className="my-2 flex flex-col gap-1">
+        <VarComp level="1" />
+        <VarComp level="2" />
+        <VarComp level="3" />
+      </div>
     </div>
   );
 }
