407f6268c3c9123474383acc13814f809940e659
aric <1290657123@qq.com>
2024-04-28 21:30:03 +0800

    feat: add abs center

--- src/components/layout-translate-center.tsx
+++ src/components/layout-translate-center.tsx
@@ -9,14 +9,21 @@ export default () => {
     <>
       <div className="debug-red relative block size-80">
         <h1>Dialog center</h1>
-        <div className="layout-translate-center absolute size-16 rounded-xl bg-green-400"></div>
+        <div className="layout-translate-center absolute size-16 rounded-xl bg-green-400">
+          center
+        </div>
       </div>
       <div className="debug-red relative block size-80">
         <h1>Layout Translate Center</h1>
-        <div className="layout-translate-center absolute size-16 rounded-xl bg-green-400"></div>
-        <div className="layout-translate-center-xy absolute size-16 rounded-xl bg-amber-400"></div>
-        <div className="layout-translate-center-y absolute size-16 rounded-full bg-blue-400"></div>
-        <div className="layout-translate-center-x absolute top-0 size-16 rounded-3xl bg-red-400"></div>
+        <div className="layout-translate-center-xy absolute size-16 rounded-xl bg-amber-400">
+          xy
+        </div>
+        <div className="layout-translate-center-y absolute size-16 rounded-full bg-blue-400">
+          y
+        </div>
+        <div className="layout-translate-center-x absolute top-0 size-16 rounded-3xl bg-red-400">
+          x
+        </div>
       </div>
     </>
   );
