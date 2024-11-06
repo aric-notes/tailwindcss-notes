ac7d90c657c7bda58911e36aa5fa1fde59db0c83
aric <1290657123@qq.com>
2024-11-06 13:36:58 +0800

    feat: add layout-flex-scrollable

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import FfDemo from './components/ff-demo.tsx';
-import WithPropsDemo from './components/with-props-demo.tsx';
-import FFVariantGroups from './components/ff-variant-groups.tsx';
+import LayoutFlexPage from './components/layout-flex-page.tsx';
 
 function App() {
-  return (
-    <div className="bg-gray-200 p-10">
-      <div className="debug-red y-2">
-        <WithPropsDemo title="With Props Dem1o" />
-        <WithPropsDemo
-          title="With Props Dem2o"
-          description="This is a description 12345"
-        />
-      </div>
-      <FfDemo />
-      <div className="my-5">
-        <FFVariantGroups />
-      </div>
-    </div>
-  );
+  return <LayoutFlexPage />;
 }
 
 export default App;
---
+++ src/components/layout-flex-page.tsx
@@ -0,0 +1,61 @@
+import { FC } from 'react';
+
+const Anonymous: FC = () => {
+  return (
+    <div className="layout-flex-scrollable h-150">
+      <header className="h-10 bg-slate-200">HEADER</header>
+      <div className="content is-flex-scrollable-y">
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+        <p>
+          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+        </p>
+      </div>
+      <footer className="h-10 bg-amber-50">
+        Copyright © 2021 Start-Auto. All rights reserved.
+      </footer>
+    </div>
+  );
+};
+
+export default Anonymous;
