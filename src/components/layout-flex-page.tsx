54698215f0a83bd60139dd3cb4eeba8a7a1c852a
aric <1290657123@qq.com>
2025-03-15 15:14:23 +0800

    feat: ios 13 worked

--- src/components/layout-flex-page.tsx
+++ src/components/layout-flex-page.tsx
@@ -2,56 +2,17 @@ import { FC } from 'react';
 
 const Anonymous: FC = () => {
   return (
-    <div className="layout-flex-scrollable h-150">
+    <div className="layout-flex-scrollable debug-red h-150 p-5">
       <header className="h-10 bg-slate-200">HEADER</header>
       <div className="content is-flex-scrollable-y">
         <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
-        </p>
-        <p>
-          <img src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg" />
+          <img
+            src="https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg"
+            className="h-20"
+          />
         </p>
       </div>
-      <footer className="h-10 bg-amber-50">
+      <footer className="h-5 bg-amber-50">
         Copyright © 2021 Start-Auto. All rights reserved.
       </footer>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
+import babel from '@rollup/plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: [
+        ['@babel/preset-env', { targets: '> 0.5%, last 2 versions, not dead' }]
+      ],
+      plugins: [
+        '@babel/plugin-proposal-optional-chaining',
+        '@babel/plugin-proposal-nullish-coalescing-operator'
+      ]
+    }),
+    legacy({
+      targets: ['defaults', 'ios>=12', 'ChromeAndroid >= 66'],
+      additionalLegacyPolyfills: ['core-js/stable'], // 确保引入 `core-js`
+      modernPolyfills: true
+    })
+  ],
+  server: {
+    host: '0.0.0.0'
+  },
+  define: {
+    _global: {}
+  }
+});
