68e43c589773a6cda7e781c6000dd5e996bbe27f
aric <1290657123@qq.com>
2025-08-15 19:29:45 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import cx from 'classnames';
 function App() {
   return (
     <div>
-      <fieldset className="layout-fieldset-divider text-center text-gray-300">
-        <legend className="px-4 text-black">OR</legend>
+      <fieldset className="layout-fieldset-divider border-gray-300 text-center">
+        <legend className="px-4">OR</legend>
       </fieldset>
       <div>
         <h3 className="sm:text-2xl md:text-3xl lg:text-4xl">
@@ -25,6 +25,39 @@ function App() {
           <div className="item6">item7</div>
           <div className="item7">item8</div>
           <div className="item8">item9</div>
+        </div>
+        <div className="layout-flex-list-[3,5,4] lg:layout-flex-list-[4,3,4] xl:layout-flex-list-[5,3,3] *:size-20 *:bg-gray-200">
+          <div className="item">item1</div>
+          <div className="item2">item2</div>
+          <div className="item3">item3</div>
+          <div className="item4">item4</div>
+          <div className="item5">item5</div>
+          <div className="item6">item7</div>
+          <div className="item7">item8</div>
+          <div className="item8">item9</div>
+          <div className="item9">item10</div>
+        </div>
+        <div className="layout-float-list-[3,5,4] mx-auto w-200 *:h-20 *:bg-slate-200">
+          <div className="item">item1</div>
+          <div className="item2">item2</div>
+          <div className="item3">item3</div>
+          <div className="item4">item4</div>
+          <div className="item5">item5</div>
+          <div className="item6">item7</div>
+          <div className="item7">item8</div>
+          <div className="item8">item9</div>
+          <div className="item9">item10</div>
+        </div>
+        <div className="list1 *:size-20 *:bg-gray-200">
+          <div className="item">item1</div>
+          <div className="item2">item2</div>
+          <div className="item3">item3</div>
+          <div className="item4">item4</div>
+          <div className="item5">item5</div>
+          <div className="item6">item7</div>
+          <div className="item7">item8</div>
+          <div className="item8">item9</div>
+          <div className="item9">item10</div>
         </div>
       </div>
       <LayoutFlexPage />
