f88f808eb3dd2f637da9ed4d2cc510f279d7e2e1
aric <1290657123@qq.com>
2025-08-17 11:29:26 +0800

    feat: test lc-x

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import cx from 'classnames';
 
 function App() {
   return (
-    <div>
+    <div className="wkit-scrollbar-w-1 debug-blue h-screen overflow-x-auto border-2">
+      <div className="mx-auto max-w-50 text-center lc-2">
+        清晨醒来，打开窗帘，一抹慵懒的阳光照进来，暖暖的，柔柔的，时光瞬间变得温婉静美，打开音乐，沏一杯花茶，躺在床上，暖阳淼淼，茶香淡淡，音乐袅袅，闭上眼睛，嘴角轻轻上扬，算是对着光阴的镜子，和自己撒个娇。
+      </div>
       <fieldset className="layout-fieldset-divider border-gray-300 text-center">
         <legend className="px-4">OR</legend>
       </fieldset>
